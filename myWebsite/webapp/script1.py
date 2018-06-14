7b5c7f3c81ec44ed02f1c19d577892ad9e389422
Sahil Sharma <sahil.sharma@sjsu.edu>
2018-06-13 21:37:22 -0700

    content modified for dockerization

--- myWebsite/webapp/script1.py
+++ myWebsite/webapp/script1.py
@@ -27,4 +27,5 @@ def about():
 # If we import this script from another script, then __name__ will be equals to __script1__
 # Script is whole code from line1 to last line.
 if __name__ == "__main__":
-	app.run(debug = True)
+	app.run(host='0.0.0.0')
+#	app.run(debug = True)
